f6ea78b5f458c769cbb7e1f9b89c0caa6f2ea71f
= <daniel@creatio.com.au>
2013-10-16 09:19:52 +1100

    Change to allow backing up multiple accounts

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 from boto import ec2
 import time
-import ConfigParser
+import json
 from os import path
 
 start = time.time()
 
-config = ConfigParser.ConfigParser()
-config.read(path.dirname(path.abspath(__file__)) + '/credentials.ini')
-if not config.has_option('default', 'access_key_id'):
-    raise Exception('No access_key_id option')
+config_file = open(path.dirname(path.abspath(__file__)) + '/credentials.json', 'r')
+config = json.load(config_file)
 
-if not config.has_option('default', 'secret_access_key'):
-    raise Exception('No secret_access_key option')
+for account_config in config:
+    print("Backing up account %s" % account_config['name'])
+    for region in ec2.regions():
+        conn = region.connect(
+            aws_access_key_id=account_config['access_key_id'],
+            aws_secret_access_key=account_config['secret_access_key']
+        )
 
-for region in ec2.regions():
-    conn = region.connect(
-        aws_access_key_id=config.get('default', 'access_key_id'),
-        aws_secret_access_key=config.get('default', 'secret_access_key')
-    )
+        volumes = conn.get_all_volumes()
 
-    volumes = conn.get_all_volumes()
+        print("Backing up region %s, %s volumes found" % (region.name, len(volumes)))
+        for volume in volumes:
+            print(" Create snapshot for %s" % volume.id)
+            volume.create_snapshot()
 
-    print("Backing up region %s, %s volumes found" % (region.name, len(volumes)))
-    for volume in volumes:
-        print(" Create snapshot for %s" % volume.id)
-        volume.create_snapshot()
-
-    conn.trim_snapshots(hourly_backups=0, daily_backups=7, weekly_backups=4)
+        conn.trim_snapshots(hourly_backups=0, daily_backups=7, weekly_backups=4)
 
 print("Done in %.1fs" % (time.time() - start))
