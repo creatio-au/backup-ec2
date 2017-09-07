d4350f53d8fa1e6aef6e64d9f7adea25ead31558
Daniel Holmes <daniel@danielholmes.org>
2017-09-07 12:21:58 +1000

    Continue backing up when one account has error

---
+++ README.md
@@ -0,0 +1,8 @@
+# Backup AWS
+
+Small program to run through a set of AWS accounts and backup all EC2 Machines.
+
+
+## Running
+
+`python main.py`
--- main.py
+++ main.py
@@ -10,19 +10,22 @@ config = json.load(config_file)
 
 for account_config in config:
     print("Backing up account %s" % account_config['name'])
-    for region in ec2.regions():
-        conn = region.connect(
-            aws_access_key_id=account_config['access_key_id'],
-            aws_secret_access_key=account_config['secret_access_key']
-        )
+    try:
+        for region in ec2.regions():
+            conn = region.connect(
+                aws_access_key_id=account_config['access_key_id'],
+                aws_secret_access_key=account_config['secret_access_key']
+            )
 
-        volumes = conn.get_all_volumes()
+            volumes = conn.get_all_volumes()
 
-        print("Backing up region %s, %s volumes found" % (region.name, len(volumes)))
-        for volume in volumes:
-            print(" Create snapshot for %s" % volume.id)
-            volume.create_snapshot()
+            print("Backing up region %s, %s volumes found" % (region.name, len(volumes)))
+            for volume in volumes:
+                print(" Create snapshot for %s" % volume.id)
+                volume.create_snapshot()
 
-        conn.trim_snapshots(hourly_backups=0, daily_backups=7, weekly_backups=4)
+            conn.trim_snapshots(hourly_backups=0, daily_backups=7, weekly_backups=4)
+    except Exception as e:
+        print('Error encountered, continuing backups: {}'.format(repr(e)))
 
 print("Done in %.1fs" % (time.time() - start))
