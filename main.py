f61632889c2cce89f9e43e7f59c200d2f625b257
Daniel Holmes <daniel@danielholmes.org>
2017-09-07 12:37:07 +1000

    Email notification for backup aws

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from boto import ec2
+import smtplib
 import time
 import json
 from os import path
@@ -8,7 +9,8 @@ start = time.time()
 config_file = open(path.dirname(path.abspath(__file__)) + '/credentials.json', 'r')
 config = json.load(config_file)
 
-for account_config in config:
+monitoring = config.get('monitoring')
+for account_config in config.get('accounts'):
     print("Backing up account %s" % account_config['name'])
     try:
         for region in ec2.regions():
@@ -27,5 +29,10 @@ for account_config in config:
             conn.trim_snapshots(hourly_backups=0, daily_backups=7, weekly_backups=4)
     except Exception as e:
         print('Error encountered, continuing backups: {}'.format(repr(e)))
+        smtp = smtplib.SMTP(monitoring.get('host'), monitoring.get('port'))
+        smtp.starttls()
+        smtp.login(monitoring.get('username'), monitoring.get('password'))
+        smtp.sendmail(monitoring.get('sender'), monitoring.get('receiver'), 'Error in backup AWS: {}'.format(repr(e)))
+        smtp.close()
 
 print("Done in %.1fs" % (time.time() - start))
